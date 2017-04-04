24912ee2ee561f0895c7182f377b76dac34e8995
talentlian <lianbo201x@gmail.com>
2017-04-04 14:35:41 -0400

    copy files from project_1

---
+++ triangle.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import turtle
+
+painter = turtle.Turtle()
+
+for _ in range(18):
+    painter.forward(100)
+    painter.left(100)
+
+turtle.done()
+
+# In[ ]:
+
+
+
